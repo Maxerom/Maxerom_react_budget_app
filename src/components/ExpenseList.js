f86873d4553393490efc6e6f19a35285aa87b93a
Maxerom <you@example.com>
2023-08-07 11:46:48 -0400

    All done

--- src/components/ExpenseList.js
+++ src/components/ExpenseList.js
@@ -1,1 +1,24 @@
+import React, { useContext } from 'react';
+import ExpenseItem from './ExpenseItem';
+import { AppContext } from '../context/AppContext';
 
+const ExpenseList = () => {
+    const { expenses } = useContext(AppContext);
+
+    return (
+        <table className='table'>
+            <thead className="thead-light">
+                <th scope="col">Department</th>
+                <th scope="col">Allocated Budget</th>
+                <th scope="col">Increase by 10</th>
+                <th scope="col">Delete</th>
+            </thead>
+                <tbody>
+                    {expenses.map((expense) => (
+                        <ExpenseItem id={expense.id} key={expense.id} name={expense.name} cost={expense.cost} />
+                    ))}
+                </tbody>
+        </table>
+    );
+};
+export default ExpenseList;
